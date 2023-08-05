09b7648965290501528257a64ed357ec754ad83e
Pink Champagne <45930107+PinkChampagne17@users.noreply.github.com>
2023-08-05 14:12:25 +0800

    chore: migrate to monorepo

--- packages/nson-cli/index.js
+++ packages/nson-cli/index.js
@@ -5,27 +5,27 @@ const { pipeline } = require("node:stream");
 const { createInflateRaw, createDeflateRaw } = require("node:zlib");
 
 const { input, output } = program
-  .requiredOption("-i, --input <string>")
-  .requiredOption("-o, --output <string>")
-  .parse(process.argv)
-  .opts();
+	.requiredOption("-i, --input <string>")
+	.requiredOption("-o, --output <string>")
+	.parse(process.argv)
+	.opts();
 
 const transformerCtor = input.endsWith(".nson")
-  ? createInflateRaw
-  : createDeflateRaw;
+	? createInflateRaw
+	: createDeflateRaw;
 
 const transformer = transformerCtor({
-  windowBits: 15,
+	windowBits: 15,
 });
 
 const source = createReadStream(input);
 const destination = createWriteStream(output);
 
 pipeline(source, transformer, destination, (err) => {
-  if (err) {
-    console.error("An error occurred:", err);
-    process.exit(1);
-  } else {
-    console.log("Completed.");
-  }
+	if (err) {
+		console.error("An error occurred:", err);
+		process.exit(1);
+	} else {
+		console.log("Completed.");
+	}
 });
